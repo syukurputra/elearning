fe427876e266d8363e566f2f84bc6a1ec9e97998
syukur niyadi putra <syukur.putra@gmail.com>
2020-05-14 09:08:11 +0700

    setting up server

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
-const app = require('./src/app')
-require('./src/connection');
+const express = require('express');
+const app = express();
 
-async function init() {
-    await app.listen(8080);
-    console.log('Server on localhost:8080')
-}
+var PORT = process.env.PORT || 8080;
 
-init();
+app.use(express.json());
+app.use(express.urlencoded({ extended: false}))
+
+app.use(require('./src/controllers/authController'))
+
+const mongoose = require("mongoose")
+
+// const db = process.env.MONGODB_URL;
+const db = "mongodb+srv://syukur:syukur123@cluster0-y2qoe.gcp.mongodb.net/elearning?retryWrites=true&w=majority";
+
+mongoose.connect(db, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+}).then(db => console.log("Connection establishe successfully"));
+
+app.get('/', (req, res) => res.send('<h2> Welcome in elearning </h2>'));
+
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
